99471cb348fe4d561e95e9a734bd52696e438a8c
makarathong22 <bointhong90@gmail.com>
2022-10-18 21:30:34 +0700

    Fix this scret leak

--- db/migration/000001_init_schema.sql.down.sql
+++ db/migration/000001_init_schema.sql.down.sql
@@ -1,8 +1,3 @@
-DROP EXISTS tbl_menu_items;
-DROP EXISTS tbl_menu_item_details;
-DROP EXISTS tbl_menu_categories;
-DROP EXISTS tbl_menu_groups;
-DROP EXISTS tbl_menu_sizes;
-DROP EXISTS tbl_terminals;
-DROP EXISTS tbl_outlets;
-DROP EXISTS tbl_vats;
+
+
+DROP TABLE tbl_vats
